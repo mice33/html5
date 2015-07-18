f5523fee0ec92eb896423297ee814a545bdb0953
何湘红 <xhhe@Ctrip.com>
2015-07-18 18:06:16 +0800

    run index

--- MyCtripDemo/MyTour/Resource/controllers/index.js
+++ MyCtripDemo/MyTour/Resource/controllers/index.js
@@ -1,1 +1,41 @@
 ﻿console.info("into control index.js");
+
+define(['cPageView'], function (pageview) {
+
+	var self;
+    var view = pageview.extend({
+
+     	onCreate: function() {
+            self = this;
+        },
+
+        setHead:function(){
+        	var self = this;
+        	this.header.set({
+        		title: 'My Tour Index',
+		        back: true,
+		        home: true,
+		        tel: {
+		          number: 4000086666
+		        },
+		        events: {
+		          returnHandler: function () {
+		          	console.info("click back button");
+		          },
+		          homeHandler: function () {
+		          	console.info("click home button");
+		          }
+		        }
+        	});
+        },
+
+        onShow: function () {
+        	self.setHead();
+        }
+
+    });
+
+
+    return view;
+
+});
